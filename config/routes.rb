9c4e6711ad8bba9dcde404c4d4935f4776eac960
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2021-02-04 13:28:43 +0900

    resourcesを使ったCRUD処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  #新規投稿画面への遷移
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  delete '/users/:id', to: 'users#destroy'
-  get '/users/:id', to: 'users#edit'
-  patch '/users/:id', to: 'users#update'
+  resources :users
 end
